645e757b8a4e539746a75bf5768c82570147ba86
JPeer264 <jan.oster94@gmail.com>
2017-05-15 21:23:28 +0200

    Feat: support windows

--- index.js
+++ index.js
@@ -20,21 +20,31 @@ const gitNeedsPull = (altPath = cwd, { remote, branch } = {}) => {
   b = b && b.slice(0, 14) !== '(HEAD detached' ? b : 'master';
 
   try {
+    let localCommitExec;
+    let remoteCommitExec;
+    let lastCommonCommitExec;
+
     if (platform() === 'win32') {
-      execa.shellSync(`pushd ${thisPath} & git pull ${r} ${b} --dry-run --no-verify`);
+      localCommitExec = `pushd ${thisPath} & git rev-parse "@")`;
+      remoteCommitExec = `pushd ${thisPath} & git rev-parse ${r}/${b})`;
+      lastCommonCommitExec = `pushd ${thisPath} & git merge-base "@" ${r}/${b})`;
     } else {
-      const localCommit = execa.shellSync(`(cd ${thisPath} ; git rev-parse "@")`).stdout;
-      const remoteCommit = execa.shellSync(`(cd ${thisPath} ; git rev-parse ${r}/${b})`).stdout;
+      localCommitExec = `(cd ${thisPath} ; git rev-parse "@")`;
+      remoteCommitExec = `(cd ${thisPath} ; git rev-parse ${r}/${b})`;
+      lastCommonCommitExec = `(cd ${thisPath} ; git merge-base "@" ${r}/${b})`;
+    }
 
-      if (localCommit === remoteCommit) {
-        return false;
-      }
+    const localCommit = execa.shellSync(localCommitExec).stdout;
+    const remoteCommit = execa.shellSync(remoteCommitExec).stdout;
 
-      const lastCommonCommit = execa.shellSync(`(cd ${thisPath} ; git merge-base "@" ${r}/${b})`).stdout;
+    if (localCommit === remoteCommit) {
+      return false;
+    }
 
-      if (lastCommonCommit === localCommit) {
-        return true;
-      }
+    const lastCommonCommit = execa.shellSync(lastCommonCommitExec).stdout;
+
+    if (lastCommonCommit === localCommit) {
+      return true;
     }
 
     return false;

38cc2bf5061754594e58176ab752cccb23b16f72
JPeer264 <jan.oster94@gmail.com>
2020-03-19 09:14:03 +0100

    Fix: update branch before check if it needs to pull

--- index.js
+++ index.js
@@ -20,19 +20,24 @@ const gitNeedsPull = (altPath = cwd, { remote, branch } = {}) => {
   b = b && b.slice(0, 14) !== '(HEAD detached' ? b : 'master';
 
   try {
+    let updateExec;
     let localCommitExec;
     let remoteCommitExec;
     let lastCommonCommitExec;
 
     if (platform() === 'win32') {
+      updateExec = `pushd ${thisPath} & git fetch"`;
       localCommitExec = `pushd ${thisPath} & git rev-parse "@"`;
       remoteCommitExec = `pushd ${thisPath} & git rev-parse ${r}/${b}`;
       lastCommonCommitExec = `pushd ${thisPath} & git merge-base "@" ${r}/${b}`;
     } else {
+      updateExec = `(cd ${thisPath} ; git fetch)`;
       localCommitExec = `(cd ${thisPath} ; git rev-parse "@")`;
       remoteCommitExec = `(cd ${thisPath} ; git rev-parse ${r}/${b})`;
       lastCommonCommitExec = `(cd ${thisPath} ; git merge-base "@" ${r}/${b})`;
     }
+
+    execa.shellSync(updateExec);
 
     const localCommit = execa.shellSync(localCommitExec).stdout;
     const remoteCommit = execa.shellSync(remoteCommitExec).stdout;
